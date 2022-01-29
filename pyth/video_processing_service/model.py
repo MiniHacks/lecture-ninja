f6544eeae9bb039583a54936aaa34f50957d24a3
Ritik Mishra <ritik.mishra314@gmail.com>
2022-01-29 15:58:11 -0600

    add a bunch of pydantic classes for modelling textbooks

---
+++ pyth/video_processing_service/model.py
@@ -0,0 +1,49 @@
+from enum import Enum
+from typing import List, Literal, Optional
+from pydantic import BaseModel
+
+# Equivalent/intended rust definition
+# 
+# enum TextbookElement {
+#     Paragraph(String),
+#     Figure {
+#         image_url: String,
+#         caption: Option<String>
+#     },
+#     Heading(String)
+#     Section {
+#         contents: Vec<TextbookElement>
+#     }
+# }
+
+class TextbookElementKindEnum(Enum):
+    PARAGRAPH = "paragraph"
+    FIGURE = "figure"
+    HEADING = "heading"
+    SECTION = "section"
+
+
+class TextbookElement(BaseModel):
+    kind: TextbookElementKindEnum
+    timestamp: float
+
+class ParagraphSegment(BaseModel):
+    text: str
+    timestamp: float
+
+class Paragraph(TextbookElement):
+    kind: Literal[TextbookElementKindEnum.PARAGRAPH] = TextbookElementKindEnum.PARAGRAPH
+    contents: List[ParagraphSegment]
+
+class Figure(TextbookElement):
+    kind: Literal[TextbookElementKindEnum.FIGURE] = TextbookElementKindEnum.FIGURE
+    image_url: str
+    caption: Optional[str]
+
+class Heading(TextbookElement):
+    kind: Literal[TextbookElementKindEnum.HEADING] = TextbookElementKindEnum.HEADING
+    text: str
+
+class Section(TextbookElement):
+    kind: Literal[TextbookElementKindEnum.SECTION] = TextbookElementKindEnum.SECTION
+    contents: List[TextbookElement]
