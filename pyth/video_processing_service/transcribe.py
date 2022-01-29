b073f09b3505fe724cb76d24875dac59f92b50aa
iCalculated <fufa0001@gmail.com>
2022-01-29 15:40:41 -0600

    Local file transcription

--- pyth/video_processing_service/transcribe.py
+++ pyth/video_processing_service/transcribe.py
@@ -4,11 +4,6 @@ from google.cloud import speech
 # Instantiates a client
 client = speech.SpeechClient()
 
-# The name of the audio file to transcribe
-gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
-
-audio = speech.RecognitionAudio(uri=gcs_uri)
-
 config = speech.RecognitionConfig(
     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
     sample_rate_hertz=16000,
@@ -16,13 +11,25 @@ config = speech.RecognitionConfig(
     enable_word_time_offsets=True
 )
 
+# Local file
+import io
+speech_file = "~/Downloads/brooklyn_bridge.raw"
+with io.open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+audio = speech.RecognitionAudio(content=content)
+
+# The name of the audio file to transcribe
+gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+audio = speech.RecognitionAudio(uri=gcs_uri)
+
 # Detects speech in the audio file
 response = client.recognize(config=config, audio=audio)
 
 for result in response.results:
     alternative = result.alternatives[0]
-    print('Transcript: {}'.format(alternative.transcript))
-    print('Confidence: {}'.format(alternative.confidence))
+    # print(f'Alternatives: {list(map(lambda x: x.words, result.alternatives))}')
+    print(f'Transcript: {alternative.transcript}')
+    print(f'Confidence: {alternative.confidence}')
 
     for word_info in alternative.words:
         word = word_info.word
