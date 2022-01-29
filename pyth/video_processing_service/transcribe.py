138ba5d1b8aba7870dcff1b8d2c33a2b471e51f7
iCalculated <fufa0001@gmail.com>
2022-01-29 15:44:27 -0600

    woo, both files

--- pyth/video_processing_service/transcribe.py
+++ pyth/video_processing_service/transcribe.py
@@ -13,10 +13,26 @@ config = speech.RecognitionConfig(
 
 # Local file
 import io
-speech_file = "~/Downloads/brooklyn_bridge.raw"
+speech_file = "/Users/shy/Downloads/brooklyn_bridge.raw"
 with io.open(speech_file, "rb") as audio_file:
         content = audio_file.read()
 audio = speech.RecognitionAudio(content=content)
+
+# Detects speech in the audio file
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    alternative = result.alternatives[0]
+    # print(f'Alternatives: {list(map(lambda x: x.words, result.alternatives))}')
+    print(f'Transcript: {alternative.transcript}')
+    print(f'Confidence: {alternative.confidence}')
+
+    for word_info in alternative.words:
+        word = word_info.word
+        start_time = word_info.start_time
+        end_time = word_info.end_time
+        print(f'Word: {word}, start_time: {start_time}, end_time: {end_time}')
+
 
 # The name of the audio file to transcribe
 gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
