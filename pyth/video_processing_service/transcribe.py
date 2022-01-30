37d8383bb15c391e95bbfafc2c62c4a4b641d2a3
iCalculated <fufa0001@gmail.com>
2022-01-29 21:12:41 -0600

    blobwork

--- pyth/video_processing_service/transcribe.py
+++ pyth/video_processing_service/transcribe.py
@@ -1,25 +1,30 @@
-from google.cloud import speech
+from google.cloud import speech, storage
 import ffmpeg
 import io
-
-"""
-stream = ffmpeg.input('ted.mp4')
-stream = ffmpeg.hflip(stream)
-stream = ffmpeg.output(stream, 'flip.mp4')
-ffmpeg.run(stream)
-"""
-
 # Instantiates a client
+
+fin = 'ted.mp4'
+stem = fin.split(".")[0]
+fout = stem + ".wav"
 
 stream = ffmpeg.input('ted.mp4')
 # c=:a refers to just the audio channel, vn=None enables a binary ignore video flag
-stream = ffmpeg.output(stream.audio, "out.m4a", acodec="copy", vn=None)
+stream = ffmpeg.output(stream.audio, fout, vn=None, y=None)
 ffmpeg.run(stream)
 
-speech_file = "out.m4a"
-with io.open(speech_file, "rb") as audio_file:
-        content = audio_file.read()
-audio = speech.RecognitionAudio(uri="gs://covert_goose_videos/google_test")
+storage_client = storage.Client()
+    bucket = storage_client.bucket("covert_goose_videos")
+    blob = bucket.blob(f"{stem}_wav_blob")
+
+    blob.upload_from_filename(stem + ".wav")
+
+    print(
+        f"File {} uploaded to {}.".format(
+            stem + ".wav", destination_blob_name
+        )
+
+
+audio = speech.RecognitionAudio(uri="gs://covert_goose_videos/test_wav")
 
 client = speech.SpeechClient()
 
@@ -33,9 +38,10 @@ diarization_config = speech.SpeakerDiarizationConfig(
 config = speech.RecognitionConfig(
     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
     enable_automatic_punctuation=True,
-    sample_rate_hertz=44100,#16000,
+    sample_rate_hertz=44100,
     language_code="en-US",
     enable_word_time_offsets=True,
+    audio_channel_count=2,
     diarization_config=diarization_config,
     model="video"
 )
@@ -43,10 +49,20 @@ config = speech.RecognitionConfig(
 # Detects speech in the audio file
 #gcs_uri = "gs://covert_goose_videos/test.wav"
 #audio = speech.RecognitionAudio(uri=gcs_uri)
+print(u"Waiting for operation to complete...")
 operation = client.long_running_recognize(config=config, audio=audio)
-response = operation.result(timeout=600)
+
+response = operation.result(timeout=60)
+print("Operation ends!")
 
 print(response)
+for i, result in enumerate(response.results):
+        alternative = result.alternatives[0]
+        print("-" * 20)
+        print("First alternative of result {}".format(i))
+        print(u"Transcript: {}".format(alternative.transcript))
+
+"""
 for result in response.results:
     alternative = result.alternatives[0]
     # print(f'Alternatives: {list(map(lambda x: x.words, result.alternatives))}')
@@ -59,3 +75,4 @@ for result in response.results:
         end_time = word_info.end_time
         speaker = word_info.speaker_tag
         print(f'Word: {word}, start_time: {start_time}, end_time: {end_time}')
+"""
