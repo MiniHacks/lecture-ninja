ac36da2611f1e443674528056cb58f4f2238bb7a
Ritik Mishra <ritik.mishra314@gmail.com>
2022-01-30 11:00:32 -0600

    add playback bar to lectureid page

--- frontend/pages/lecture/[lectureid].js
+++ frontend/pages/lecture/[lectureid].js
@@ -1,20 +1,21 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {useRouter} from "next/router";
+import { useEffect, useState } from 'react';
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import Typography from "@mui/material/Typography";
 import Words from "../../components/Words";
+import PlaybackBar from '../../components/PlaybackBar';
 
 export default function lecture() {
     const router = useRouter();
     console.log(router.query);
-    const {lectureid} = router.query
+    const { lectureid } = router.query
 
-    const [data, setData] = useState({notLoaded: true});
+    const [data, setData] = useState({ notLoaded: true });
     const [info, setInfo] = useState({})
     const [time, setTime] = useState(0);
     useEffect(() => {
-        if(!lectureid) return;
+        if (!lectureid) return;
         fetch(process.env.NEXT_PUBLIC_BACKEND + "/lecture/" + lectureid).then(r => r.json()).then(r => {
             setData(r);
         })
@@ -22,25 +23,30 @@ export default function lecture() {
             setInfo(r)
         })
     }, [lectureid])
-    return (<Layout>
+    return (<>
+        <Layout>
 
-        {JSON.stringify(data)}
+            {JSON.stringify(data)}
 
-        {JSON.stringify(info)}
+            {JSON.stringify(info)}
 
-        <Typography variant={"h1"} mt={4}>
-            {info.class} - {info.lecture}
-        </Typography>
+            <Typography variant={"h1"} mt={4}>
+                {info.class} - {info.lecture}
+            </Typography>
 
-        <Typography variant={"body1"} mt={2}>
-            Summary for: {info.class} - {info.lecture} - {time}
-        </Typography>
-        {parser(data, setTime)}
-    </Layout>);
+            <Typography variant={"body1"} mt={2}>
+                Summary for: {info.class} - {info.lecture} - {time}
+            </Typography>
+            {parser(data, setTime)}
+        </Layout>
+        <PlaybackBar secondsElapsed={10} secondsTotal={11} paused={false} videoTitle={
+            `${info.class} - ${info.lecture}`
+        }></PlaybackBar>
+    </>);
 }
 
 const parser = (item, setTime) => {
-    switch(item.kind){
+    switch (item.kind) {
         case "section":
             return item.contents.map(a => parser(a, setTime));
         case "heading":
@@ -48,8 +54,8 @@ const parser = (item, setTime) => {
         case "figure":
             return <figure onClick={() => setTime(item.timestamp)}>
                 <img src={item.image_url}
-                     alt={item.caption}/>
-                    <figcaption>{item.caption}</figcaption>
+                    alt={item.caption} />
+                <figcaption>{item.caption}</figcaption>
             </figure>
         case "paragraph":
             return item.contents.map(a => <Words onClick={() => setTime(a.timestamp)} speaker={a.speaker_tag} body={a.text} />)
