9ddde3a2fffcf29958fc01b70e66a757cd5ae681
minizou <gh.mini.zou@gmail.com>
2022-01-29 19:31:51 -0600

    Add cards to dashboard

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -10,35 +10,39 @@ import { CardActionArea } from '@mui/material';
 
 export default function CustomCard() {
     return (
-        <Card sx={{ maxWidth: 1060 }}>
+        <Card sx={{ maxWidth: 1060, borderRadius: 2, m: "1rem" }}>
             <CardActionArea>
                 <CardContent>
-                    {/* card image */}
-                    <CardMedia
-                        component="img"
-                        width="164"
-                        height="100"
-                        image="./thumbnail_placeholder.png"
-                        alt="lecture thumbnail"
-                    />
-                    {/* info section */}
+                    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
 
-                    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Typography gutterBottom variant="h5" component="div">
-                            This is a card title.
-                        </Typography>
-                        <Typography gutterBottom variant="h5" component="div">
-                            #1
-                        </Typography>
+                        {/* card image */}
+                        <CardMedia sx={{ mr: "1rem", borderRadius: 2 }}
+                            component="img"
+                            width="164"
+                            image="./thumbnail_placeholder.png"
+                            alt="lecture thumbnail"
+                        />
+
+                        {/* info section */}
+                        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    This is a card title.
+                                </Typography>
+                                <Typography gutterBottom variant="h5" color="text.secondary" component="div">
+                                    #1
+                                </Typography>
+                            </Box>
+                            <Typography variant="body2" gutterBottom>
+                                This is a card description. This is a card description.
+                                This is a card description. This is a card description.
+                                This is a card description.
+                            </Typography>
+                            <Typography sx={{ fontSize: 12 }} color="text.secondary">
+                                Uploaded April 1, 2022 at 12:14 AM
+                            </Typography>
+                        </Box>
                     </Box>
-                    <Typography variant="body2" color="text.secondary" gutterBottom>
-                        This is a card description. This is a card description.
-                        This is a card description. This is a card description.
-                        This is a card description.
-                    </Typography>
-                    <Typography sx={{ fontSize: 12 }} color="text.secondary">
-                        Uploaded April 1, 2022 at 12:14 AM
-                    </Typography>
                 </CardContent>
             </CardActionArea>
         </Card>
