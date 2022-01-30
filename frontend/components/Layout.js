f994788cf23f283ff20ed3bf5887d9f0e5724955
Samyok Nepal <samyok@fiveable.me>
2022-01-29 23:54:02 -0600

    params to customcard

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,7 +2,7 @@ import Box from "@mui/material/Box";
 
 const Layout = ({children}) => {
     return <Box display={"flex"} justifyContent={"center"}>
-        <Box mx={"max(2px, 5%)"} maxWidth={"1500px"} width={"100%"}>
+        <Box mx={"max(80px, 5%)"} maxWidth={"1500px"} width={"100%"}>
             {children}
         </Box>
     </Box>
