9de7f51dc33813015543d81d208bbb53a9c3c6a9
Samyok Nepal <samyok@fiveable.me>
2022-01-29 23:38:32 -0600

    fix layout

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,8 +1,10 @@
 import Box from "@mui/material/Box";
 
 const Layout = ({children}) => {
-    return <Box mx={"max(2px, 5%)"}>
-        {children}
+    return <Box display={"flex"} justifyContent={"center"}>
+        <Box mx={"max(2px, 5%)"} maxWidth={"1500px"} width={"100%"}>
+            {children}
+        </Box>
     </Box>
 }
 export default Layout;
