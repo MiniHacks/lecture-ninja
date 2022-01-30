5515ac67d125b5b233b45a0a1fc26e5b90f61577
Samyok Nepal <samyok@fiveable.me>
2022-01-29 19:32:36 -0600

    add login stuff

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,25 +1,28 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ProTip from '../src/ProTip';
-import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import LoginModal from "../components/Modals/Login";
 
 export default function About() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Button variant="contained" component={Link} noLinkStyle href="/">
-          Go to the main page
-        </Button>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{my: 4}}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Next.js example
+                </Typography>
+                <Button variant="contained" onClick={() => setShowLoginModal(true)}>
+                    Go to the main page
+                </Button>
+                <ProTip/>
+                <Copyright/>
+            </Box>
+            {showLoginModal && <LoginModal isLogin={true} handleClose={() => setShowLoginModal(false)}/>}
+        </Container>
+    );
 }
