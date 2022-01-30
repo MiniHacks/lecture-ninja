cc53ed2359dea9858a18e8cc643179e00d9977e9
minizou <gh.mini.zou@gmail.com>
2022-01-29 20:05:00 -0600

    Adding text to landing page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,24 +7,33 @@ import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
 export default function Index() {
-  return (
-    <Layout>
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h2" component="h1" gutterBottom>
-          Did you ever wish you could’ve rewinded an IRL
-          lecture?
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <div>
-          <Link href="/dashboard" color="secondary">
-            dashboard page
-          </Link>
-        </div>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Box sx={{ my: 4 }}>
+
+                {/* text */}
+                <Typography variant="h2" component="h1" gutterBottom>
+                    Did you ever wish you could’ve rewinded an IRL
+                    lecture?
+                </Typography>
+                <Typography variant="body1" component="h1" gutterBottom>
+                    we transcribe videos into digestible sections with timestamps, images, and bolded key points or something wooo. we make things accessible
+                </Typography>
+
+                {/* image */}
+
+
+                <Link href="/about" color="secondary">
+                    Go to the about page
+                </Link>
+                <div>
+                    <Link href="/dashboard" color="secondary">
+                        dashboard page
+                    </Link>
+                </div>
+                <ProTip />
+                <Copyright />
+            </Box>
+        </Layout>
+    );
 }
