e1fc57f230e394f3dff2741526368a3fd1c3171c
minizou <gh.mini.zou@gmail.com>
2022-01-29 22:11:43 -0600

    Reformat dashboard

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {useState} from 'react';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
