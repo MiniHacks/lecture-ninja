004913140dcac1814ba905c31007599ebe1d9c60
Samyok Nepal <samyok@fiveable.me>
2022-01-29 23:38:20 -0600

    fix dashboard

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -2,20 +2,20 @@ import * as React from 'react';
 import CustomCard from '../components/Card';
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
+import Layout from "../components/Layout";
 
 
 export default function Dashboard() {
   return (
     <>
-      <Box sx={{ ml: "3.75rem" }}> {/* FIXME */}
-          {/* delete later - just meant as a  */}
-          <Skeleton sx={{ maxWidth: 1360,  mx: "1rem"}} animation="wave">
+      <Layout>
+          <Skeleton sx={{ my: "1rem"}} animation="wave">
             <CustomCard />
           </Skeleton>
           <CustomCard />
           <CustomCard />
           <CustomCard />
-      </Box>
+      </Layout>
     </>
   );
 }
