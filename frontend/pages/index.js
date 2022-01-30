089e82b311f7a4016a87b59999daac778f362df1
minizou <gh.mini.zou@gmail.com>
2022-01-29 21:24:19 -0600

    Add footer, style landing page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,56 +1,63 @@
 import * as React from 'react';
 import {useState} from 'react';
-import Layout from '../components/Layout';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import LoginModal from "../components/Modals/Login";
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Index() {
     const [showLoginModal, setShowLoginModal] = useState(false);
     let isLogin = false;
     return (
-        <Layout>
-            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', my: 4 }}>
+        <>
+            <Navbar />
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                minHeight: "70vh",
+                mx: 14,
+                my: 4 }}
+            >
                 {/* text */}
-                <Box> 
+                <Box sx={{ mx: "2.5rem" }} > 
                     {/* FIXME: add navbar */}
-
-                    <Typography variant="h2" component="h1" gutterBottom>
+                    <Typography fontFamily="Fredoka One" fontWeight="medium" sx={{ my: "2rem" }} variant="h1" component="h1" gutterBottom>
                         Did you ever wish you could’ve rewinded an IRL
                         lecture?
                     </Typography>
-                    <Typography variant="body1" component="h1" gutterBottom>
+                    
+                    <Typography sx={{ my: "2rem" }} variant="body1" component="h1" gutterBottom>
                         we transcribe videos into digestible sections with timestamps, images, and bolded key points or something wooo. we make things accessible
                     </Typography>
 
-                    {/* button */}
-                        <Button variant="contained" onClick={() => {setShowLoginModal(true)}}>
+                    <Box> 
+                        <Button sx={{ mr: "1rem", px: "2rem" }} variant="contained" onClick={() => {setShowLoginModal(true)}}>
                             Start
                         </Button>
-                        <Button variant="outlined" onClick={() => {setShowLoginModal(true); isLogin=true}}>
-                        Login
+                        <Button sx={{ px: "2rem" }} variant="outlined" onClick={() => {
+                            setShowLoginModal(true);
+                            isLogin = true;
+                        }}>
+                            Login
                         </Button>
-
-                    {/* delete later */}
-                    <div>
-                        <Link href="/about" color="secondary">
-                            Go to the about page |
-                        </Link>
-                        <Link href="/dashboard" color="secondary">
-                            | dashboard page
-                        </Link>
-                    </div>
+                    </Box>
                 </Box>
 
-                <Box sx={{}}>
-                    <img src="../img/mascot.png" alt="ninja goose" />
-                </Box>
+                <Box
+                    component="img"
+                    sx={{
+                    height: 450,
+                    width: 495,
+                    }}
+                    alt="ninja goose"
+                    src="../img/mascot.png"
+                />
             </Box>
             {showLoginModal && <LoginModal handleClose={() => setShowLoginModal(false)} isLogin={isLogin} />}
-        </Layout>
+            <Footer />
+        </>
     );
 }
